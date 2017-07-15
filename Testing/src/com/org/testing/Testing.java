9c47065d55cf0e626f8d83903415dd1836298535
vk1993 <visalrao19@gmail.com>
2017-07-15 20:17:28 +0530

    committed the project Please Look Over it

---
+++ Testing/src/com/org/testing/Testing.java
@@ -0,0 +1,20 @@
+package com.org.testing;
+public class Testing{
+	
+	 public static void main(String[] arg)
+	    {
+	        Rectangle re = new Rectangle();
+	        re.draw();
+	    }   
+}
+
+ class Rectangle implements Drawable{
+	  public void draw(){
+		 
+		 System.out.println("drawawa");
+		 
+	 }
+ }
+interface Drawable{  
+ void draw();
+} 
